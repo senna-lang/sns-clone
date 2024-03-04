dec42f77d41c2bcbac5e803c97c362216c4d2927
senna-lang <asrsena3302@gmail.com>
2024-03-04 15:14:27 +0900

    session情報に応じてログイン画面にリダイレクト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import AuthButtonServer from '@/components/AuthButtonServer';
+import { redirect } from 'next/navigation';
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
   const { data: posts } = await supabase.from('Posts').select();
+  const session = await supabase.auth.getSession();
+  if (!session) {
+    redirect('/login');
+  }
   // console.log(posts)
   return (
     <div>
