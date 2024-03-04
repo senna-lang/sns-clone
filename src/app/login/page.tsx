fc7029340a933eb900e389b395124a5ce4c1f37f
senna-lang <asrsena3302@gmail.com>
2024-03-04 15:14:45 +0900

    loginページを作成

---
+++ src/app/login/page.tsx
@@ -0,0 +1,23 @@
+import AuthButtonServer from '@/components/AuthButtonServer';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+const Login = async () => {
+  const supabase = createServerComponentClient({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session) {
+    redirect('/');
+  }
+
+  return (
+    <div>
+      <AuthButtonServer />
+    </div>
+  );
+};
+
+export default Login;
