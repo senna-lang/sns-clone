8abf7d95cf05511af4282192f3421e8d4352c419
senna-lang <asrsena3302@gmail.com>
2024-03-04 23:17:24 +0900

    postフォーム実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,19 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import AuthButtonServer from '@/components/AuthButtonServer';
 import { redirect } from 'next/navigation';
+import PostForm from '@/components/Post';
 
 export default async function Home() {
   const supabase = createServerComponentClient<Database>({ cookies });
   const { data: posts } = await supabase.from('Posts').select();
+  console.log(posts)
   const session = await supabase.auth.getSession();
   if (!session) {
     redirect('/login');
   }
   return (
     <div>
+      <PostForm/>
       <AuthButtonServer />
     </div>
   );
---
+++ src/components/Post.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import { Button } from './ui/button';
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from './ui/form';
+import { Input } from './ui/input';
+import { ToastContainer, toast } from 'react-toastify';
+import { useEffect } from 'react';
+import 'react-toastify/dist/ReactToastify.css';
+import { usePostForm } from '@/hooks/usePostForm';
+
+const PostForm = () => {
+  const { form, onSubmit } = usePostForm();
+
+  useEffect(() => {
+    if (form.formState.isSubmitSuccessful) {
+      toast.success('メール送信完了');
+    }
+  }, [form.formState.isSubmitSuccessful]);
+
+  return (
+    <Form {...form}>
+      <ToastContainer />
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="container flex flex-col gap-3"
+      >
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>ユーザーネーム</FormLabel>
+              <FormControl>
+                <Input placeholder="title" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" disabled={form.formState.isSubmitting}>
+          {form.formState.isSubmitting ? '送信中' : '送信'}
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default PostForm;
