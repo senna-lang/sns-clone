8a7f6dd805fba9701ade00a38e5a05fedcdfeba3
senna-lang <asrsena3302@gmail.com>
2024-03-05 22:43:48 +0900

    Postの型をグローバル化

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,12 @@
 import { Database as SupabaseDB } from './lib/database.types';
 declare global {
   type Database = SupabaseDB;
+  type Post = Database['public']['Tables']['posts']['Row'];
+  type Profile = Database['public']['Tables']['profile']['Row'];
+
+  type PostsWithAuthor = Post & {
+    profile: Profile;
+    likes: number;
+    user_has_liked_post: boolean;
+  };
 }
