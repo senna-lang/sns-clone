e809a1960e25a2d5e58f0b944ac953ef539cdc10
senna-lang <asrsena3302@gmail.com>
2024-03-05 15:32:01 +0900

    型付け

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,16 +1,18 @@
 'use server';
 
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { revalidatePath } from 'next/cache';
 import { cookies } from 'next/headers';
 
 export const newPost = async (formData: FormData) => {
   try {
-    const supabase = createServerComponentClient({ cookies });
-    const title = formData.get('title');
+    const supabase = createServerComponentClient<Database>({ cookies });
+    const title = String(formData.get('title'));
     const {
       data: { user },
     } = await supabase.auth.getUser();
     await supabase.from('posts').insert({ title, user_id: user?.id });
+    revalidatePath('/');
   } catch (err) {
     console.error(err);
   }
