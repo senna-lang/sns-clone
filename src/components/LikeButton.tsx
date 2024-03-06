fa260f435b4a05b05956d3290a5f18c225e4634f
senna-lang <asrsena3302@gmail.com>
2024-03-06 13:26:54 +0900

    いいね機能に楽観的UI更新を実装

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -2,28 +2,49 @@
 
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import React, { useTransition } from 'react';
 
-const LikeButton = ({ post }: { post: PostsWithAuthor }) => {
+const LikeButton = ({
+  post,
+  addOptimisticPosts,
+}: {
+  post: PostsWithAuthor;
+  addOptimisticPosts: (newPost: PostsWithAuthor) => void;
+}) => {
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
   const handleClick = async () => {
     const supabase = createClientComponentClient<Database>();
     const {
       data: { user },
     } = await supabase.auth.getUser();
     if (post.user_has_liked_post) {
+      addOptimisticPosts({
+        ...post,
+        likes: post.likes - 1,
+        user_has_liked_post: !post.user_has_liked_post,
+      });
       await supabase
         .from('likes')
         .delete()
         .match({ user_id: user?.id, post_id: post.id });
     } else {
+      addOptimisticPosts({
+        ...post,
+        likes: post.likes + 1,
+        user_has_liked_post: !post.user_has_liked_post,
+      });
       await supabase
         .from('likes')
         .insert({ user_id: user?.id, post_id: post.id });
-      router.refresh();
     }
+    router.refresh();
   };
-  return <button onClick={handleClick}>{post.likes}いいね</button>;
+  return (
+    <button onClick={() => startTransition(() => handleClick())}>
+      {post.likes}いいね
+    </button>
+  );
 };
 
 export default LikeButton;
--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
+'use client';
+import React, { useOptimistic } from 'react';
 import LikeButton from './LikeButton';
 
-
-const PostsList = ({ posts }: {posts:PostsWithAuthor[]}) => {
-  // console.log(posts)
+const PostsList = ({ posts }: { posts: PostsWithAuthor[] }) => {
+  const [optimisticPosts, addOptimisticPosts] = useOptimistic<
+    PostsWithAuthor[],
+    PostsWithAuthor
+  >(posts, (currentOptimisticPosts, newPosts) => {
+    const newOptimisticPosts = [...currentOptimisticPosts];
+    const index = newOptimisticPosts.findIndex(post => post.id === newPosts.id);
+    newOptimisticPosts[index] = newPosts;
+    return newOptimisticPosts;
+  });
   return (
     <div>
-      {posts?.map(post => (
+      {optimisticPosts?.map(post => (
         <div key={post.id}>
           <p>
             {post.profile?.name}
             {post.profile?.username}
           </p>
           <p>{post.title}</p>
-          <LikeButton post={post} />
+          <LikeButton post={post} addOptimisticPosts={addOptimisticPosts} />
         </div>
       ))}
     </div>
